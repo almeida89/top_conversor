684d00f4528c8819288ec778f7c05e4a410e21dd
Fabio Almeida <fabio_almeida89@outlook.com>
2025-11-03 22:47:17 -0300

    refactor(index): Refatorando estrutura para deixar mais limpa e profissional

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
-// Nome do Arquivo: pages/index.js
-
-import Converter from '../components/converter';
+import Converter from '../components/Converter';
 import Head from 'next/head';
 
 export default function HomePage() {
   return (
-    // Um contêiner de layout básico
-    <div className="flex justify-center items-center min-h-screen bg-gray-900 text-white">
+    // Este div agora só precisa centralizar o conteúdo na página.
+    // O 'min-h-screen' aqui garante que o conteudo seja centralizado
+    // verticalmente na tela inteira.
+    <div className="flex justify-center items-center min-h-screen p-4">
       <Head>
-        <title>Top Conversor</title>
-        <meta name="description" content="Conversor universal de medidas" />
+        <title>Top Conversor - Início</title>
       </Head>
 
-      <main>
-        {/* Aqui é onde o seu componente principal é renderizado */}
-        <Converter />
-      </main>
+      <Converter />
     </div>
   );
 }
